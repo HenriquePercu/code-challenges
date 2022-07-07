c6df54815fec24d52311cdacc3abe0b9002fa142
Henrique Percu <henrique.percu@gmail.com>
2022-07-07 00:25:06 -0300

    Refactor packages

---
+++ src/codewars/ArrayDiff.java
@@ -0,0 +1,45 @@
+package codewars;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+class Kata {
+
+    public static int[] arrayDiff(int[] a, int[] b) {
+        ArrayList<Integer> diffList = new ArrayList<>();
+
+        outer:
+        for (int originalArrayNumber : a) {
+
+            for (int numberToRemove : b) {
+                if (originalArrayNumber == numberToRemove) {
+                    continue outer;
+                }
+            }
+            diffList.add(originalArrayNumber);
+        }
+
+        return diffList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
+}
+
+
+public class ArrayDiff {
+
+    @Test
+    public void sampleTests() {
+        assertArrayEquals(new int[]{2}, Kata.arrayDiff(new int[]{1, 2}, new int[]{1}));
+        assertArrayEquals(new int[]{2, 2}, Kata.arrayDiff(new int[]{1, 2, 2}, new int[]{1}));
+        assertArrayEquals(new int[]{1}, Kata.arrayDiff(new int[]{1, 2, 2}, new int[]{2}));
+        assertArrayEquals(new int[]{1, 2, 2}, Kata.arrayDiff(new int[]{1, 2, 2}, new int[]{}));
+        assertArrayEquals(new int[]{}, Kata.arrayDiff(new int[]{}, new int[]{1, 2}));
+    }
+
+}

b278df0f0a6df33031a3a7ca43f16dbceb18f30b
Henrique Percu <henrique.percu@gmail.com>
2022-07-13 00:00:37 -0300

    New exercise from codechef

--- src/leetcode/LongestSubstringWithoutRepeating3.java
+++ src/leetcode/LongestSubstringWithoutRepeating3.java
@@ -5,32 +5,36 @@ import java.util.HashMap;
 public class LongestSubstringWithoutRepeating3 {
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring("tmmzuxt"));
     }
 
+    //TODO instead manipulating a substring, it could be only two index iterating over a char[] marking the substring, so we dont need the lastImportantIndex
     public static int lengthOfLongestSubstring(String s) {
-        return findLongestSubstringRecursive(s);
-    }
-
-    private static int findLongestSubstringRecursive(String s) {
-        String maximumSubstring = "";
+        HashMap<Character, Integer> lettersOnTheSubsequence = new HashMap<>();
+        String longestSubstring = "";
         String validateSubstring = "";
+        int lastImportanteIndex = 0;
+
 
         for (int i = 0; i < s.length(); i++) {
-            Character character = s.charAt(i);
-            if (!validateSubstring.contains(String.valueOf(character))) {
+            char character = s.charAt(i);
+            if (lettersOnTheSubsequence.get(character) == null || lettersOnTheSubsequence.get(character) < lastImportanteIndex) {
+                lettersOnTheSubsequence.put(character, i);
                 validateSubstring = validateSubstring.concat(String.valueOf(character));
             } else {
-                validateSubstring = validateSubstring.substring(validateSubstring.indexOf(character) + 1).concat(String.valueOf(character));
+                if (i != s.length() - 1) {
+                    lastImportanteIndex = lettersOnTheSubsequence.get(character) + 1;
+                    validateSubstring = s.substring(lastImportanteIndex, i).concat(String.valueOf(character));
+                    lettersOnTheSubsequence.put(character, i);
+                }
             }
 
-            if (validateSubstring.length() > maximumSubstring.length()) {
-                maximumSubstring = validateSubstring;
+            if (longestSubstring.length() < validateSubstring.length()) {
+                longestSubstring = validateSubstring;
             }
-
         }
 
-        return maximumSubstring.length();
+        return longestSubstring.length();
     }
 
 }

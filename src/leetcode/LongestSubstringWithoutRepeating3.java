d007ac90ffc777ef81699afb9114d38a81253bb3
Henrique Percu <henrique.percu@gmail.com>
2022-07-12 23:49:36 -0300

    New exercise from codechef

---
+++ src/leetcode/LongestSubstringWithoutRepeating3.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeating3 {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        return findLongestSubstringRecursive(s);
+    }
+
+    private static int findLongestSubstringRecursive(String s) {
+        String maximumSubstring = "";
+        String validateSubstring = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            Character character = s.charAt(i);
+            if (!validateSubstring.contains(String.valueOf(character))) {
+                validateSubstring = validateSubstring.concat(String.valueOf(character));
+            } else {
+                validateSubstring = validateSubstring.substring(validateSubstring.indexOf(character) + 1).concat(String.valueOf(character));
+
+            }
+
+            if (validateSubstring.length() > maximumSubstring.length()) {
+                maximumSubstring = validateSubstring;
+            }
+
+        }
+
+        return maximumSubstring.length();
+    }
+
+}

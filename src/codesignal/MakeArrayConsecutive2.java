6bcd05ecdfd6cd3367f277f39082b840220400f0
henriquepercudeoliveira <henrique.percu@gmail.com>
2023-05-03 16:46:01 -0300

    Solving problem from code signal

---
+++ src/codesignal/MakeArrayConsecutive2.java
@@ -0,0 +1,26 @@
+package codesignal;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Solving this challenge for Code Signal : <a href="https://app.codesignal.com/arcade/intro/level-2/bq2XnSr5kbHqpHGJC">Problem</a>
+ */
+public class MakeArrayConsecutive2 {
+
+    int solution(int[] statues) {
+        Arrays.sort(statues);
+        int difference = 0;
+
+        for (int i = 0; i < statues.length - 1; i++) {
+
+            if (statues[i + 1] - statues[i] > 1) {
+                difference += statues[i + 1] - statues[i] - 1;
+            }
+
+        }
+        return difference;
+
+    }
+
+}

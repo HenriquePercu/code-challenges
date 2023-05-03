b20fc1e0f9d70188fe21845df890d27cf10869ec
henriquepercudeoliveira <henrique.percu@gmail.com>
2023-05-03 17:05:13 -0300

    Solving problem from code signal

--- src/codesignal/AlmostIncreasingSequence.java
+++ src/codesignal/AlmostIncreasingSequence.java
@@ -5,20 +5,35 @@ package codesignal;
  */
 public class AlmostIncreasingSequence {
 
-    boolean solution(int[] sequence) {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1,2,1,2}));
+    }
+
+    static boolean solution(int[] sequence) {
         int numErr = 0;
         for (int i = 0; i < sequence.length - 1; i++) {
-            if (sequence[i] - sequence[i+1] >= 0) {
-                numErr += 1;
-                if (i - 1 >= 0 && i + 2 <= sequence.length - 1
-                        && sequence[i] - sequence[i+2] >= 0
-                        && sequence[i-1] - sequence[i+1] >= 0) {
-                    return false;
+            if (sequence[i] - sequence[i + 1] >= 0) {
+                // Test whether stepping back or forwards can bridge the hump or pothole
+                var noStepBack = i - 1 >= 0 && sequence[i - 1] >= sequence[i + 1];
+                var noStepForward = i + 2 < sequence.length && sequence[i] >= sequence[i + 2];
+
+                // We only test for bridge gaps when i > 0
+                if (i > 0 && noStepBack && noStepForward) {
+                    // Cannot step back over gap forwards or backwards
+                    // Counts as two errors ONLY WHEN BOTH PRESENT
+                    numErr += 2;
+                } else {
+                    // Typical error
+                    numErr++;
                 }
+            }
+
+            if (numErr > 1) {
+                return false;
             }
         }
 
-        return numErr <= 1;
+        return true;
     }
 
 }

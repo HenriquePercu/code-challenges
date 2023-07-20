6a94a55aa558f4c7a5943e44c39add80d2e116ec
henriquepercudeoliveira <henrique.percu@gmail.com>
2023-07-19 21:30:09 -0300

    Solving problem from Code Signal

---
+++ src/codesignal/ArrayReplace.java
@@ -0,0 +1,15 @@
+package codesignal;
+
+public class ArrayReplace {
+
+    int[] solution(int[] inputArray, int elemToReplace, int substitutionElem) {
+
+        for (int i = 0; i < inputArray.length; i++) {
+
+            if (inputArray[i] == elemToReplace) {
+                inputArray[i] = substitutionElem;
+            }
+        }
+        return inputArray;
+    }
+}

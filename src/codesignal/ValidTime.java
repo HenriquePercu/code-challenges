53e956f22ced18055f34ea9d65b252acd981f50c
henriquepercudeoliveira <henrique.percu@gmail.com>
2024-02-28 23:09:20 -0300

    Solving problem from Code Signal

---
+++ src/codesignal/ValidTime.java
@@ -0,0 +1,19 @@
+package codesignal;
+
+/**
+ * Solving this challenge for Code Signal : <a href="https://app.codesignal.com/arcade/intro/level-12/ywMyCTspqGXPWRZx5">Problem</a>
+ */
+public class ValidTime {
+
+    boolean solution(String time) {
+
+        String[] hourParts = time.split(":");
+
+        int hour = Integer.parseInt(hourParts[0]);
+        int minute = Integer.parseInt(hourParts[1]);
+
+        return hour > 0 && hour <= 24 && minute > 0 && minute < 59;
+    }
+
+
+}

b8ea978771e5bf13316ce51aae6e9fbfc5377ebc
henriquepercudeoliveira <henrique.percu@gmail.com>
2024-04-18 23:08:41 -0300

    Solving problem from Leet Code

---
+++ src/leetcode/RotateArray.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+        rotate(new int[]{1, 2}, 5);
+    }
+
+    public static void rotate(int[] nums, int k) {
+
+        while (k > nums.length) {
+            k = k - nums.length;
+        }
+
+        List<Integer> firstSlice = new java.util.ArrayList<>(Arrays.stream(nums, nums.length - k, nums.length).boxed().toList());
+        List<Integer> secondSlice = Arrays.stream(nums, 0, nums.length - k).boxed().toList();
+
+        firstSlice.addAll(secondSlice);
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = firstSlice.get(i);
+        }
+    }
+
+}

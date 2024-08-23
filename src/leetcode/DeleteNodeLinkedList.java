c7f396f7115c16e370b6dd91942a044ec5520a3d
henriquepercudeoliveira <henrique.percu@gmail.com>
2024-08-22 23:46:03 -0300

    Solving problem from Leet Code

--- src/leetcode/DeleteNodeLinkedList.java
+++ src/leetcode/DeleteNodeLinkedList.java
@@ -1,11 +1,12 @@
 package leetcode;
+
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/553/
 public class DeleteNodeLinkedList {
 
     public void deleteNode(ListNode node) {
-        while (node.next != null) {
-            node = node.next;
-        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+
     }
 
 
